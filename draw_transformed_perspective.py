c5aa572bbd1dad960c33bf3b700737837b10a340
Moein <moein.akbari2003@gmail.com>
2024-11-07 14:09:49 +0330

    fixed: sorted corners to match with perspective.

--- draw_transformed_perspective.py
+++ draw_transformed_perspective.py
@@ -3,12 +3,44 @@
 
 from impact_utils import detect_markers
 
-from typing import Sequence
+from typing import Sequence, Tuple
 
 import numpy as np 
 import cv2
 from cv2.typing import MatLike 
 
+
+def __find_center(marker_corners: Sequence[MatLike]) -> Tuple[float, float]:
+    # The Sequence only contains one item
+    marker_corners = marker_corners[0]
+    total = np.array([0, 0])
+    for corner in marker_corners:
+        total += corner
+    return total / len(marker_corners)
+    
+    
+def sort_markers(markers_corners: Sequence[MatLike]) -> Sequence[MatLike]:
+    # Grouping two left makers and two right markers
+    markers_corners = sorted(
+        markers_corners,
+        key=lambda x: __find_center(x)[0]
+    )
+
+    left_markers = markers_corners[0:1]
+    right_markers = markers_corners[2:3]
+
+    # Sorting based on height
+    left_markers = sorted(
+        left_markers,
+        key=lambda x: __find_center(x)[1]
+    )
+    
+    right_markers = sorted(
+        right_markers,
+        key=lambda x: __find_center(x)[1]
+    )
+
+    return np.array([left_markers, right_markers])
 
 def draw_transformed_perspective(
         frame: Sequence[MatLike],
@@ -16,9 +48,12 @@ def draw_transformed_perspective(
     ) -> MatLike: 
 
     PERSPECTIVE = np.float32(
-        [[0, 0], [400, 0],
-        [0, 640], [400, 640]]
+        [[0, 0], [0, 450],
+        [300, 0], [300, 450]]
     )
+
+    corners = sort_markers(corners)
+
     points = np.array([corner[0][0] for corner in corners])
     print(points)
     squeezed_points =np.squeeze(points)
@@ -33,7 +68,7 @@ while True:
 
     if not ret or cv2.waitKey(24) == 27:
         break
-    corners, _, frame = detect_markers(frame, draw_markers=True)
+    corners, ids, frame = detect_markers(frame, draw_markers=True)
     cv2.imshow('frame', frame)
 
     if len(corners) < 4:
